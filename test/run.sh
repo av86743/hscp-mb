c24a28767f9f6edef534db54476b40559786132f
Alex V <av86743@users.noreply.github.com>
2023-02-18 07:06:08 +0000

    resolver: ghc-7.10.2

---
+++ test/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+suites='
+trivial toys
+jason neil peter
+imaginary
+compile-time
+'
+
+test -r blacklist || exit 11
+
+for suite in $suites ;do
+
+for f in examples/"$suite"/*.core ;do
+
+t=${f#examples/}
+t=${t%.core}
+grep -qe "^${t}$" blacklist && continue
+
+stack exec hscp-mb-exe -- -v0 "$f"
+
+done
+
+done
